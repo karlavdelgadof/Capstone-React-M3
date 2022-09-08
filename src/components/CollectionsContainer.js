29739299172d4dbe9e98eb2e4a941ae10cc3a27c
karlavdelgadof <karlavdelgadof@gmail.com>
2022-09-08 17:38:10 -0500

    Implement useNavigate for each item

--- src/components/CollectionsContainer.js
+++ src/components/CollectionsContainer.js
@@ -1,26 +1,42 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
 import { getCollections } from '../redux/Collections/collections';
 import SingleCollection from './SingleCollection';
+import '../CSS/Collections.css' 
+import { getSingleCollection } from '../redux/Collections/collections';
+
+
 
 const CollectionsContainer = () => {
   const collections = useSelector((state) => state.collections);
-  
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     if (!collections.length) {
       dispatch(getCollections());
     }
   }, []);
 
+  const handleClick = (e) => {
+    dispatch(getSingleCollection(e.key));
+    navigate(e.to);
+  };
+
   return (
     <div className="collections-cont">
       <h2>Collections</h2>
-      <ul id="collections">
-        {collections.map((collection) => {
-          <SingleCollection key={collection.id} collection={collection}/>
-        })}
-      </ul>
+      <div id="collections">
+        { 
+          collections.map((collection) => (
+            <Link to={`/${collection.id}`} className="single-collection" key={collection.id} >
+            <SingleCollection
+              onClick={handleClick}
+              collection={collection} 
+            />
+            </Link>
+        ))}
+      </div>
     </div>
   );
 };
