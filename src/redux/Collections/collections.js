6e9dfb45c6a455ef3a6e658a82ad1b75fe1d4abf
karlavdelgadof <karlavdelgadof@gmail.com>
2022-09-08 17:40:04 -0500

    Create reducer with API calls

--- src/redux/Collections/collections.js
+++ src/redux/Collections/collections.js
@@ -1,29 +1,39 @@
 import axios from 'axios';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const baseURL = 'https://opensea13.p.rapidapi.com/collections';
-
 const initialState = [];
 
 const options = {
   method: 'GET',
   url: 'https://opensea13.p.rapidapi.com/collections',
-  params: {offset: '0', limit: '20'},
+  params: {offset: '0', limit: '10'},
   headers: {
     'X-RapidAPI-Key': 'bdca063a2bmshfeefa33d27bc218p136a9djsnfe57cdf02e74',
     'X-RapidAPI-Host': 'opensea13.p.rapidapi.com'
   }
 };
 
-
-
 export const getCollections = createAsyncThunk('collections/getCollections', async () => {
-  const response = await axios.request(options).then(function (response) {
-}).catch(function (error) {
-	console.error(error);
+  const response = await axios.request(options).then((response) => response.data)
+  return response;
 });
-  return response.data;
+
+export const getSingleCollection = createAsyncThunk('collection/getSingleCollection', 
+async (id) => {
+  const optionsTwo = {
+    method: 'GET',
+    url: `https://opensea13.p.rapidapi.com/collection/${id}`,
+    headers: {
+      'X-RapidAPI-Key': 'bdca063a2bmshfeefa33d27bc218p136a9djsnfe57cdf02e74',
+      'X-RapidAPI-Host': 'opensea13.p.rapidapi.com'
+    }
+  };
+  
+  const response = await axios.request(optionsTwo).then((response) => 
+  console.log(response.data))
+  return response;
 });
+
 
 export const collectionSlice = createSlice({
   name: 'collections',
@@ -32,13 +42,17 @@ export const collectionSlice = createSlice({
   extraReducers: (builder) => {
     //Add reducers to handle loading state as needed
     builder
-      .addCase(getCollections.fulfilled, (state, action) => action.payload.map(
+      .addCase(getCollections.fulfilled, (state, action) => action.payload.collections.map(
         (collection) => ({
-          id: collection.created_date,
+          id: collection.slug,
           name: collection.name,
           src: collection.image_url,
         }),
-      ));
+      ))
+      .addCase(getSingleCollection.fulfilled, (state, action) => {
+        console.log(action.payload.collection)
+        action.payload.collection
+      })
   },
 });
 
