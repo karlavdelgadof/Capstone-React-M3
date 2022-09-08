55f4187d7e1c7311ceb4e98688d5240d3d32ddc9
karlavdelgadof <karlavdelgadof@gmail.com>
2022-09-08 17:08:12 -0500

    Install redux logger

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import collectionsReducer from './Collections/collections';
+import logger from 'redux-logger';
 
 const store = configureStore({
   reducer: {
     collections: collectionsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger), 
 });
 
 export default store;
