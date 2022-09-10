882a4348bc3c0d90ed81fb25b8e9e8a9aa8b33ea
karlavdelgadof <karlavdelgadof@gmail.com>
2022-09-09 22:58:59 -0500

    Correct linter errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/configureStore";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
@@ -14,5 +14,5 @@ root.render(
         <App />
       </Provider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
