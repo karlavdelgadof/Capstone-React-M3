f2a613863381f00bac15329682fa3fd61bfa60f0
karlavdelgadof <karlavdelgadof@gmail.com>
2022-09-09 22:57:06 -0500

    Add search/filter functionality

--- src/components/CollectionsContainer.js
+++ src/components/CollectionsContainer.js
@@ -1,43 +1,56 @@
-import React, { useEffect } from 'react';
+/* eslint-disable-line react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, Link } from 'react-router-dom';
-import { getCollections } from '../redux/Collections/collections';
+import { Link } from 'react-router-dom';
+import { getCoins } from '../redux/Coins/coins';
 import SingleCollection from './SingleCollection';
-import '../CSS/Collections.css' 
-import { getSingleCollection } from '../redux/Collections/collections';
-
-
+import '../CSS/Collections.css';
+import searchIcon from '../multimedia/search-icon.png';
+// import '../CSS/Collection.css';
 
 const CollectionsContainer = () => {
-  const collections = useSelector((state) => state.collections);
+  const coins = useSelector((state) => state.coins);
+  const [search, setSearch] = useState('');
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   useEffect(() => {
-    if (!collections.length) {
-      dispatch(getCollections());
+    if (!coins.length) {
+      dispatch(getCoins());
     }
   }, []);
 
-  const handleClick = (e) => {
-    dispatch(getSingleCollection(e.key));
-    navigate(e.to);
-  };
+  const filtered = coins.filter(
+    (coin) => !search || new RegExp(search, 'ig').test(coin.name),
+  );
 
   return (
-    <div className="collections-cont">
-      <h2>Collections</h2>
-      <div id="collections">
-        { 
-          collections.map((collection) => (
-            <Link to={`/${collection.id}`} className="single-collection" key={collection.id} >
-            <SingleCollection
-              onClick={handleClick}
-              collection={collection} 
-            />
-            </Link>
-        ))}
+    <>
+      <div id="search">
+        <input
+          type="search"
+          value={search}
+          placeholder="Search Collection"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+        <i><img src={searchIcon} alt="" /></i>
       </div>
-    </div>
+      <div className="collections-cont">
+        <h2>Collections</h2>
+        <div id="collections">
+          {filtered.map((coin) => (
+            <Link
+              to="details"
+              state={{ coin }}
+              className="single-collection"
+              key={coin.id}
+            >
+              <SingleCollection coin={coin} />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
