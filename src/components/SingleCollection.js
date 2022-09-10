4c21a53e38c4053c18ae39c6da77a4d16c5c2b33
karlavdelgadof <karlavdelgadof@gmail.com>
2022-09-09 22:57:30 -0500

    Create single (homepage) coin item

--- src/components/SingleCollection.js
+++ src/components/SingleCollection.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import statsIcon from '../multimedia/stats1.png';
 // import { useDispatch } from 'react-redux';
 
 const SingleCollection = (props) => {
-  const { collection } = props;
-  // const dispatch = useDispatch();
-  // console.log('SingleCollection',collection)
-  // const handleClick = (e) => {
-  //   dispatch(joinMissions({ id: e.target.id }));
-  // };
-
+  const { coin } = props;
   return (
     <div>
-      <div className="collection-img">
-        <img src={collection.src ? collection.src : '../multimedia/default-image.jpg'}/>
+      <div className="collection-info">
+        <h2 className="single-collection-title">{coin.name}</h2>
+        <span>{coin.symbol}</span>
       </div>
-      
-      <span className='single-collection-title'>{collection.name}</span>
+      <span>
+        {`#${coin.rank}`}
+        <i><img src={statsIcon} alt="" /></i>
+      </span>
     </div>
   );
 };
 
 SingleCollection.propTypes = {
-  collection: PropTypes.shape({
-    src: PropTypes.string,
+  coin: PropTypes.shape({
+    rank: PropTypes.string,
     name: PropTypes.string,
+    symbol: PropTypes.string,
   }).isRequired,
 };
 
